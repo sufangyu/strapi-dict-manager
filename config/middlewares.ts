36071387a7d7a421afd1e48c1b5f5a6276b1ce89
sufangru <sufangru@zj.tech>
2026-03-16 15:31:34 +0800

    feat: 支持自定义请求头（跨域问题）

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -4,7 +4,20 @@ const config: Core.Config.Middlewares = [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: '*',
+      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept', 'x-custom', 'x-custom-interceptor'],
+      // // 自动允许请求中带的所有自定义请求头
+      // headers: (ctx) => {
+      //   // 浏览器预检请求带的 Access-Control-Request-Headers
+      //   return ctx.request.header['access-control-request-headers']?.split(',').map(h => h.trim()) || [];
+      // },
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS', 'HEAD'],
+      credentials: true,
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
